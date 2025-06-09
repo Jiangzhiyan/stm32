76f1abffea23ffb2fd0a24a47402a3f4323e5145
jiangzhiyan <gliker123@icloud.com>
2025-06-09 17:02:38 +0800

    新建文件

---
+++ cmake-build-debug/CMakeFiles/stm32.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "stm32.map"
+  "CMakeFiles/stm32.dir/Core/Src/main.c.obj"
+  "CMakeFiles/stm32.dir/Core/Src/main.c.obj.d"
+  "CMakeFiles/stm32.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/stm32.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "CMakeFiles/stm32.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/stm32.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "CMakeFiles/stm32.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/stm32.dir/Core/Src/syscalls.c.obj.d"
+  "CMakeFiles/stm32.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/stm32.dir/Core/Src/sysmem.c.obj.d"
+  "CMakeFiles/stm32.dir/startup_stm32f103xe.s.obj"
+  "stm32.elf"
+  "stm32.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/stm32.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
